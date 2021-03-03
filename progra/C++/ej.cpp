5c2b857470fd1345b583a82624bbd13e8c8cbc84
f4bian-pacheco <fabianlinux@LAPTOP-VC38951I.localdomain>
2021-03-03 11:32:57 -0300

    arreglando y probando ycm_extra_config.py
    Committer: f4bian-pacheco <fabianlinux@LAPTOP-VC38951I.localdomain>

--- progra/C++/ej.cpp
+++ progra/C++/ej.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-
+#include <vector>
 
 int main(){
     std::cout << "prueba \r\n\r\n"<< std::endl;
     std::cout << "cambio" << std::endl;
-
-
-
+    std::vector<int> hola = {1,2,3,4,};
 }

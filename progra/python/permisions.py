27ac00a735e31592a5820b9c89812016826dad69
f4bian-pacheco <fabian.paxe@gmail.com>
2022-02-13 13:37:08 -0300

    permision.py added

--- progra/python/permisions.py
+++ progra/python/permisions.py
@@ -14,3 +14,19 @@ The permisions of a file in a Linux system are split into three sets
  755 is read/write/execute for the owner, read/execute for the group
  and others; converted to a string, it would be:"rwxr-xr-x"
 """
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+
+    for num_permision in [int(n) for n in str(octal)]:
+
+        for value, letter in value_letters:
+            if num_permision >= value:
+                result += letter
+                num_permision -= value
+            else:
+                result += "-"
+    return result
+
+print(octal_to_string(755))
